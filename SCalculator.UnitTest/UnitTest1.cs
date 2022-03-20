cc75ed6836d521672725253bb30cf1a9671653c0
Kamil <kamil-klawi@wp.pl>
2022-03-20 13:21:41 +0100

    New variable has been added to test Added_Argument_Test

--- SCalculator.UnitTest/UnitTest1.cs
+++ SCalculator.UnitTest/UnitTest1.cs
@@ -5,14 +5,15 @@ namespace SCalculator.UnitTest
 {
     public class Tests
     {
-        [TestCase ("x", 15, "x 15")]
-        [TestCase ("y", 25, "y 25")]
-        [TestCase ("z", 35, "z 35")]
+        [TestCase ("x", 15, "x = 15")]
+        [TestCase ("y", 25, "y = 25")]
+        [TestCase ("z", 35, "z = 35")]
         public void Added_Argument_Test(string setName, double setValue, string expectedResult)
         {
             var calculator = new Calculator();
 
-            var result = calculator.AddArgument(setName, setValue);
+            calculator.AddArgument(setName, setValue);
+            var result = calculator.ShowAllArguments();
 
             Assert.AreEqual(expectedResult, result);
         }
